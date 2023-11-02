f88cee0f0b76ef13b3f65077ef66b826188c158a
rdgronii <rgroner2@gmail.com>
2023-11-02 11:53:14 -0500

    loading a specific kind of Codable data

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ricky David Groner II on 11/2/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+    
+    
+}
