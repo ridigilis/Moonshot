92b00c36967b4bccabcd3efd6f3e4027d21bf9a1
rdgronii <rgroner2@gmail.com>
2023-11-02 11:37:15 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ricky David Groner II on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
